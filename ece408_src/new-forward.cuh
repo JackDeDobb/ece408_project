88899c4dad1ef0a55772c092d8d087a2ffc8d94c
xinbowu2 <17jasonwu@gmail.com>
2019-04-17 16:50:46 -0500

    fix a boundary checking bu

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -5,13 +5,18 @@
 #include <mxnet/base.h>
 
 #define BLOCK_WIDTH 32
+#define MASK_WIDTH 24 * 12 * 7 * 7
+
+
 
 namespace mxnet
 {
 namespace op
 {
 
-__global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
+__constant__ float Mask[MASK_WIDTH];
+
+__global__ void forward_kernel(float *y, const float *x, const int B, const int M, const int C, const int H, const int W, const int K)
 {
 
     /*
@@ -32,7 +37,7 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
     // y4d(0,0,0,0) = a
     #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
     #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-    #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+    #define k4d(i3, i2, i1, i0) Mask[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
     int n, m, h, w, c, p, q;
 
 
@@ -71,6 +76,7 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     //CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
 
     // Extract the tensor dimensions into B,M,C,H,W,K
+
     const int B = x.shape_[0];
     const int M = y.shape_[1];
     const int C = x.shape_[1];
@@ -85,13 +91,15 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     const int H_grid = ceil(H_out/((1.0)*BLOCK_WIDTH));
     const int W_grid = ceil(W_out/((1.0)*BLOCK_WIDTH));
 
+    cudaMemcpyToSymbol(Mask, w.dptr_, sizeof(float)*M*C*K*K);
+
     const int Z = H_grid * W_grid;
     dim3 gridDim (B, M, Z);
     dim3 blockDim (BLOCK_WIDTH, BLOCK_WIDTH, 1);
 
 
     // Call the kernel
-    forward_kernel<<<gridDim, blockDim, 0>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<<<gridDim, blockDim, 0>>>(y.dptr_,x.dptr_, B,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
