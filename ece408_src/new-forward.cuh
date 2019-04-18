b1f8439c15144ad09b0926cce9f0b65a5f563500
Jackson DeDobbealere <jackdedobb@me.com>
2019-04-17 19:56:10 -0500

    finished shared memory optimization combined with constant memory

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -3,21 +3,19 @@
 #define MXNET_OPERATOR_NEW_FORWARD_CUH_
 
 #include <mxnet/base.h>
-
-#define BLOCK_WIDTH 32
-#define MASK_WIDTH 24 * 12 * 7 * 7
-
-
-
+#include <stdio.h>
 namespace mxnet
 {
 namespace op
 {
 
-__constant__ float Mask[MASK_WIDTH];
 
-__global__ void forward_kernel(float *y, const float *x, const int B, const int M, const int C, const int H, const int W, const int K)
-{
+#define TILE_SIZE 16
+#define KERNEL_SIZE 5
+__constant__ float Mask [KERNEL_SIZE * KERNEL_SIZE * 500]; // 50 max number of channels
+
+
+__global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K) {
 
     /*
     Modify this function to implement the forward pass described in Chapter 16.
@@ -25,11 +23,12 @@ __global__ void forward_kernel(float *y, const float *x, const int B, const int 
     The goal here is to be correct AND fast.
     We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
     */
+    extern __shared__ float shmem[];
+    float* X_shared = shmem;
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-
-    const int W_grid = ceil(W_out/((1.0)*BLOCK_WIDTH));
+    const int W_grid = ceil((1.0 * W_out) / TILE_SIZE);
 
     // An example use of these macros:
     // float a = y4d(0,0,0,0)
@@ -38,25 +37,35 @@ __global__ void forward_kernel(float *y, const float *x, const int B, const int 
     #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
     #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
     #define k4d(i3, i2, i1, i0) Mask[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+    
     int n, m, h, w, c, p, q;
-
+    int tile_width = TILE_SIZE + K - 1;
 
     n = blockIdx.x;
     m = blockIdx.y;
-    h = (blockIdx.z / W_grid) * BLOCK_WIDTH + threadIdx.y;
-    w = (blockIdx.z % W_grid) * BLOCK_WIDTH + threadIdx.x;
-
-    float acc = 0.0;
-
-    if (h >= 0 && h < H_out && w >= 0 && w < W_out){
-        for (c = 0; c < C; c++){
-            for (p = 0; p < K; p++)
-                for (q = 0; q < K; q++)
-                    acc += x4d(n, c, h + p, w + q) * k4d(m, c, p, q);
-        }
-
+    h = (blockIdx.z / W_grid) * TILE_SIZE + threadIdx.x;
+    w = (blockIdx.z % W_grid) * TILE_SIZE + threadIdx.y;
     
-        y4d(n, m, h, w) = acc;
+    float acc = 0.0;
+    for (c = 0; c < C; c++) {
+        __syncthreads();
+        for (int i = h; i < h + tile_width - threadIdx.x; i += TILE_SIZE) {
+            for (int j = w; j < w + tile_width - threadIdx.y; j += TILE_SIZE) {
+                if (j == W) { break; }
+                if (i < H && j < W) {
+                    X_shared[(i - h + threadIdx.x) * tile_width + j - w + threadIdx.y] = x4d(n, c, i, j);
+                }
+            }
+        }
+        __syncthreads();
+        for (p = 0; p < K; p++) {
+            for (q = 0; q < K; q++) {
+            acc = acc + X_shared[(threadIdx.x + p) * tile_width + threadIdx.y + q] * k4d(m, c, p, q);
+            }
+        }
+    }
+    if (h < H_out && w < W_out) {
+      y4d(n, m, h, w) = acc; 
     }
 
     #undef y4d
@@ -86,20 +95,19 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
+    
+
+    cudaMemcpyToSymbol(Mask, w.dptr_, KERNEL_SIZE * KERNEL_SIZE * M * C * sizeof(float), 0, cudaMemcpyDeviceToDevice);
+    size_t shmem_size = ((TILE_SIZE + K-1) * (TILE_SIZE + K-1) + K*K) * sizeof(float);
 
 
-    const int H_grid = ceil(H_out/((1.0)*BLOCK_WIDTH));
-    const int W_grid = ceil(W_out/((1.0)*BLOCK_WIDTH));
+    const int Z = ceil((1.0 * W_out) / TILE_SIZE) * ceil((1.0* H_out)/ TILE_SIZE);
 
-    cudaMemcpyToSymbol(Mask, w.dptr_, sizeof(float)*M*C*K*K);
+    dim3 gridDim(B, M, Z);
+    dim3 blockDim(TILE_SIZE, TILE_SIZE, 1);
 
-    const int Z = H_grid * W_grid;
-    dim3 gridDim (B, M, Z);
-    dim3 blockDim (BLOCK_WIDTH, BLOCK_WIDTH, 1);
-
-
-    // Call the kernel
-    forward_kernel<<<gridDim, blockDim, 0>>>(y.dptr_,x.dptr_, B,M,C,H,W,K);
+    //Call the kernel
+    forward_kernel<<<gridDim, blockDim, shmem_size>>>(y.dptr_, x.dptr_, w.dptr_, B, M, C, H, W, K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
